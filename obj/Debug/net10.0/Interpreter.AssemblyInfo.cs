e4524a657b00e788fea332bf309816320a91e234
Jared Chua <jaredrchua@gmail.com>
2026-09-02 16:20:07 +0800

    Fixed build and CI issues

--- obj/Debug/net10.0/Interpreter.AssemblyInfo.cs
+++ obj/Debug/net10.0/Interpreter.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Interpreter")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d136eb673790f8970e924e947568ec3393696fa4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Interpreter")]
